cfd3d76190404b4d44a6c14847554f1dfa6d7984
rootkill <rootkill.dedsec@gmail.com>
2023-10-09 18:42:58 +0530

    Updated the wc code for 4th step and 5th step of the challenge

--- 01_cc_wc/wc.c
+++ 01_cc_wc/wc.c
@@ -4,12 +4,18 @@
 #define IN 1    /* Inside a word */
 #define OUT 0   /* Outside a word */
 
+struct Result {
+  int count_res;
+  int exit_code;
+  char *file_name;
+};
+
 void print_result(int c, char *file_name) {
-  printf("%d\t%s\n", c, file_name);
+  printf("%d %s\n", c, file_name);
 }
 
 void print_valid_flags() {
-  printf("Valid Flags: '-c' '-l' '-w'\n");
+  printf("\e[1;33mValid Flags: '-c' '-l' '-w'\e[m\n");
 }
 
 void print_usage() {
@@ -21,34 +27,61 @@ void print_usage() {
   
 }
 
-void count_chars(char *file_name) {
-  long cc = 0;
-  FILE *f_reader = fopen(file_name, "r");
-
-  while (fgetc(f_reader) != EOF)
-    cc++;
-
-  print_result(cc, file_name);
+void print_error(char *file_name) {
+  printf("\e[1;31mError opening file: %s\e[m\n", file_name);
 }
 
-void count_lines(char *file_name) {
+int count_bytes(char *file_name) {
+  long cb = 0;
+  FILE *f_reader = fopen(file_name, "r");
+
+  if (f_reader == NULL) {
+    print_error(file_name);
+    return 1;
+  }
+ 
+  while (fgetc(f_reader) != EOF)
+    cb++;
+
+  fclose(f_reader);
+
+  print_result(cb, file_name);
+
+  return 0;
+}
+
+int count_lines(char *file_name) {
   long cl = 0;
   char c;
   FILE *f_reader = fopen(file_name, "r");
 
+  if (f_reader == NULL) {
+    print_error(file_name);
+    return 1;
+  }
+ 
   while ((c = fgetc(f_reader)) != EOF)
     if (c == '\n')
       ++cl;
-    
+
+  fclose(f_reader);
+  
   print_result(cl, file_name);
+
+  return 0;
 }
 
-void count_words(char *file_name) {
+int count_words(char *file_name) {
   long cw = 0;
   char c;
   int state = OUT;
   FILE *f_reader = fopen(file_name, "r");
 
+  if (f_reader == NULL) {
+    print_error(file_name);
+    return 1;
+  }
+  
   while ((c = fgetc(f_reader)) != EOF) {
     if (c == ' ' || c == '\t' || c == '\n') {
       state = OUT;
@@ -58,36 +91,88 @@ void count_words(char *file_name) {
     }
   }
 
+  fclose(f_reader);
+
   print_result(cw, file_name);
+
+  return 0;
+}
+
+int count_chars(char *file_name) {
+  long cc = 0;
+  char c;
+  FILE *f_reader = fopen(file_name, "r");
+
+  if (f_reader == NULL) {
+    print_error(file_name);
+    return 1;
+  }
+ 
+  while ((c = getc(f_reader)) != EOF) {
+    // Counting all the UTF-8 Characters
+    if ((c & 0xC0) != 0x80)
+	++cc;
+  }
+
+  fclose(f_reader);
+
+  print_result(cc, file_name);
+  
+  return 0;
 }
 
 int main(int argc, char **argv) {
   int exit_code = 0;
-  if (argc != 3) {
+  if (argc < 2 || argc > 3) {
     exit_code = 1;
     print_usage();
+    return exit_code;
   } else {
-    char *flag = argv[1];
-    char *cchar = "-c";
-    char *cline = "-l";
-    char *cword = "-w";
-
-    if (strcmp(flag, cchar) == 0) {
+    if (argc == 3) {
+      char *flag = argv[1];
+      char *cbyte = "-c";
+      char *cline = "-l";
+      char *cword = "-w";
+      char *cchar = "-m";
+      
+      if (strcmp(flag, cbyte) == 0) {
+	exit_code = count_bytes(argv[2]);
+      } else if (strcmp(flag, cline) == 0) {
+	exit_code = count_lines(argv[2]);
+      } else if (strcmp(flag, cword) == 0) {
+	exit_code = count_words(argv[2]);
+      } else if (strcmp(flag, cchar) == 0) {
+	exit_code = count_chars(argv[2]);
+      } else {
+	exit_code = 1;
+	print_valid_flags();
+      }
+    } else if (argc == 2) {
       exit_code = 0;
-      count_chars(argv[2]);
+      long cl, cw, cc, state;
+      char c;
+      state = OUT;
+      cl = cw = cc = 0;
+      FILE *f_reader = fopen(argv[1], "r");
+      if (f_reader == NULL) {
+	printf("Error opening file: %s", argv[2]);
+	exit_code = 1;
+	return exit_code;
+      }
+      while ((c = fgetc(f_reader)) != EOF) {
+	++cc;
+	if (c == '\n') {
+	  ++cl;
+	}
+	if (c == ' ' || c ==  '\n' || c == '\t') {
+	  state = OUT;
+	} else if (state == OUT) {
+	  state = IN;
+	  ++cw;
+	}
+      }
+      printf("  %ld %ld %ld %s\n", cl, cw, cc, argv[1]);
     }
-    else if (strcmp(flag, cline) == 0) {
-      exit_code = 0;
-      count_lines(argv[2]);
-    }
-    else if (strcmp(flag, cword) == 0) {
-      exit_code = 0;
-      count_words(argv[2]);
-    }
-    else {
-      exit_code = 1;
-      print_valid_flags();
-    }
-  }
+  } 
   return exit_code;
 }
